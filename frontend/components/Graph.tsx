97d72c3f9afd799e64780f631abc074ef1cbbd82
Semi-koron <6jimoto6@gmail.com>
2024-12-13 23:36:46 +0900

    feat type

--- frontend/components/Graph.tsx
+++ frontend/components/Graph.tsx
@@ -1,9 +1,52 @@
+"use client";
+import { useCallback } from "react";
+import {
+  ReactFlow,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from "@xyflow/react";
+import type { Tech } from "../src/app/types/Tech";
+
+import "@xyflow/react/dist/style.css";
+
+const techData = 
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
 const Graph = () => {
-    return (
-        <section className="bg-white p-4 rounded-lg shadow-md">
-            <h2 className="text-lg mb-4 text-black">グラフ</h2>
-        </section>
-    );
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  return (
+    <section className="bg-white p-4 rounded-lg shadow-md">
+      <h2 className="text-lg mb-4 text-black">グラフ</h2>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <MiniMap />
+        <Controls />
+        <Background />
+      </ReactFlow>
+    </section>
+  );
 };
 
 export default Graph;
---
+++ frontend/src/app/types/User.ts
@@ -0,0 +1,8 @@
+type User = {
+  userId: number;
+  userName: string;
+  masteredTech: number[];
+  learningTech: number[];
+};
+
+export type { User };
