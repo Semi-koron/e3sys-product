0af336b62c33dcc72860b138e901826af8ea35f2
Semi-koron <6jimoto6@gmail.com>
2024-12-21 17:53:27 +0900

    feat api mode

---
+++ backend/routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/test', function () {
+    //テストのテキストをjson形式で返す
+    return response()->json([
+        'message' => 'Hello, World!'
+    ]);
+});
+
+Route::post('/echo', function (Request $request) {
+    return response()->json([
+        'question' => $request->input('question')
+    ]);
+});
