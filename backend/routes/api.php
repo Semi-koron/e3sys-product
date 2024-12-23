c881db2fcd304c7a6999711887c9cd7494891fd1
Semi-koron <6jimoto6@gmail.com>
2024-12-23 19:28:00 +0900

    feat gemini endpoint

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GeminiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,4 +22,9 @@ Route::post('/echo', function (Request $request) {
     ]);
 });
 
+Route::post('/gemini-question', [GeminiController::class, 'question']);
+
+Route::get('/gemini-test', [GeminiController::class, 'geminiTest']);
+
 Route::get('/verify-token', [AuthController::class, 'verifyToken']);
+
